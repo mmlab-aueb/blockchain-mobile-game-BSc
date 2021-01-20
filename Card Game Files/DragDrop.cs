8b053b71859e5bcf790def6374970427d33afba0
Gody10 <odycaptain@gmail.com>
2021-01-20 21:23:21 +0200

    Card Game Scripts

---
+++ Card Game Files/DragDrop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public GameObject Canvas;
+    private GameObject startParent;
+    private bool isDragging = false;
+    bool isOverDropzone = false;
+    private GameObject dropZone;
+    private Vector2 startPosition;
+
+    private void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging) {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        isOverDropzone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        isOverDropzone = false;
+        dropZone = null;
+    }
+
+    public void StartDrag() {
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag() {
+        isDragging = false;
+        if (isOverDropzone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
+    }
+}
