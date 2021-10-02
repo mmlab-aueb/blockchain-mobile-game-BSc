5f02659a23d6eaeee8496a85f884e6d5738ff4bc
stoyian <ilstogian@gmail.com>
2021-10-02 13:06:20 +0300

    Added ERC20 card

--- JsServer/JsServer.js
+++ JsServer/JsServer.js
@@ -1,17 +1,18 @@
 
-import { createServer } from "net";
+const net = require("net");
 
-const server = createServer(socket => {
-    
+const server = net.createServer(socket => {
+
+
     
     socket.on("data", data => {
-        import WalletProvider from "truffle-hdwallet-provider-privkey";
+        const WalletProvider = require("@tru");
         const androidData = data.toString().split(" ");
         console.log(androidData);
         const privkey = androidData[1];
         const provider = new WalletProvider(privkey, // 12 word code for
         'https://rinkeby.infura.io/v3/59a3349980734b09b5a8bca63ecebfd0'); //rinkeby address from infura
-        import Web3 from 'web3';
+        const Web3 = require('web3');
         const web3 = new Web3();
         web3.setProvider(provider);
         const ABI = '[{"constant":false,"inputs":[{"internalType":"string","name":"_x","type":"string"}],"name":"change","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"_x","type":"string"}],"name":"payableChange","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"read","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"x","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]'
@@ -24,4 +25,4 @@ const server = createServer(socket => {
     })
 })
 
-server.listen(7666,"192.168.2.6")
+server.listen(7666,"localhost")
